092e90fe779d8c95c1939acfeb41e6f393eff6d5
JKKim98 <j.k.kim98@outlook.com>
2020-09-29 21:23:52 +0800

    update

--- updatefiles.sh
+++ updatefiles.sh
@@ -1,8 +1,9 @@
-cp ~/.bashrc 		.
-cp ~/.bash_profile 	.
-cp ~/.xprofile 		.
-cp ~/.Xresources 	.
+cp ~/.bashrc			.
+cp ~/.bash_profile		.
+cp ~/.xprofile			.
+cp ~/.Xresources		.
 
-cp -r ~/.config/bspwm 		./.config
+cp -r ~/.config/bspwm		./.config
 cp -r ~/.config/redshift	./.config
-cp -r ~/.config/sxhkd 		./.config
+cp -r ~/.config/sxhkd		./.config
+cp -r ~/.config/ranger		./.config
