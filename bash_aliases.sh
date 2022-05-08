94dcc118d8f526fadb2bab9b4581ab06c02adcab
JKKim98 <j.k.kim98@outlook.com>
2022-05-08 11:07:03 +0800

    add additional aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,3 +1,7 @@
+# add the line
+# . path/to/here/bash_aliases
+# to .bashrc
+
 alias logout='kill -9 -1'
 
 alias ll='ls -al --time-style=long-iso --color=auto'
@@ -5,3 +9,11 @@ alias ll='ls -al --time-style=long-iso --color=auto'
 alias projects='cd ~/projects'
 
 alias lfeverything='find . -type f -print0 | xargs -0 dos2unix'
+
+# git
+
+alias gitlogin='eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_ed25519'
+
+alias gaa='git add .'
+alias gds='git diff --staged'
+alias gc='git commit'
